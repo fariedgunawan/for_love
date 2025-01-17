2d99c8aac3c301d2eeb011774c6a459630312bae
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-01-18 05:31:52 +0700

    Update Sepuluh.tsx

--- for_love/src/page/Sepuluh.tsx
+++ for_love/src/page/Sepuluh.tsx
@@ -5,7 +5,7 @@ const Sepuluh = () => {
   return (
     <div className="landing bg-gradient-to-tr from-[#be185d] via-[#f472b6] to-[#fbcfe8] text-white min-h-screen">
       <div className="body flex flex-col justify-center items-center min-h-screen">
-        <img src={gif} alt="" className="w-[150px] rounded-lg mb-[30px] md:w-[200px]" />
+        <img src={gif} alt="" className="w-[200px] rounded-lg mb-[30px] md:w-[200px]" />
         <h2 className=" md:text-[22px] text-center font-semibold px-[20px] md:w-[800px]">
           dimoment ini juga aku dah mulai "ah maunya sama almaaa gamau sama siapa2" ini ditandai dengan aku badmood pas bawa farah ke gacoan mendingan aku bawa kamu dan bisa2nya kamu dateng secepet itu buat aku bawa, happy bgt
         </h2>
