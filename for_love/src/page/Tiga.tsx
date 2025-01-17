4fe364c12b0c31e7048cc8e0b9ba20b330e17198
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-01-18 05:29:40 +0700

    Update Tiga.tsx

--- for_love/src/page/Tiga.tsx
+++ for_love/src/page/Tiga.tsx
@@ -5,7 +5,7 @@ const Tiga = () => {
   return (
     <div className="landing bg-gradient-to-tr from-[#be185d] via-[#f472b6] to-[#fbcfe8] text-white min-h-screen">
       <div className="body flex flex-col justify-center items-center min-h-screen">
-        <img src={gif} alt="" className="w-[150px] rounded-lg mb-[30px] md:w-[200px]" />
+        <img src={gif} alt="" className="w-[200px] rounded-lg mb-[30px] md:w-[200px]" />
         <h2 className=" md:text-[22px] text-center font-semibold px-[20px] md:w-[800px]">
           kamu masi inget juga ga sama waktu ini kita ke smb bandung bertiga sama gani, waktu ini adalah kali keduanya aku ada moment sama kamu berdua nunggu mall buka, lagi lagi aku merasakan ada rasa yang berbeda dari orang lain, tapi
           waktu itu aku masi diem aja dan ga banyak omong heheheh maaf ya..
