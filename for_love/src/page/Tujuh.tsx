1ac3e4cfda0740f9471187326945fced164ac98d
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-01-18 05:30:56 +0700

    Update Tujuh.tsx

--- for_love/src/page/Tujuh.tsx
+++ for_love/src/page/Tujuh.tsx
@@ -5,7 +5,7 @@ const Tujuh = () => {
   return (
     <div className="landing bg-gradient-to-tr from-[#be185d] via-[#f472b6] to-[#fbcfe8] text-white min-h-screen">
       <div className="body flex flex-col justify-center items-center min-h-screen">
-        <img src={gif} alt="" className="w-[150px] rounded-lg mb-[30px] md:w-[200px]" />
+        <img src={gif} alt="" className="w-[200px] rounded-lg mb-[30px] md:w-[200px]" />
         <h2 className=" md:text-[22px] text-center font-semibold px-[20px] md:w-[800px]">gatau farah yang cupu ato gimana ko bisa bisanya kita sama sama 5 porsi, apakah jodoh sudah tercium dari kuali gultik?</h2>
         <button className="btn h-[10px] px-[20px] bg-white border-none hover:bg-[#f472b6] mt-[30px] text-[#be185d]" onClick={() => navigate("/8")}>
           next
