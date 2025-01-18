bd77841cd5ca1d9ee00aefbf19e72bcf39365e6c
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-01-18 12:36:08 +0700

    Update Tigabelas.tsx

--- for_love/src/page/Tigabelas.tsx
+++ for_love/src/page/Tigabelas.tsx
@@ -5,7 +5,7 @@ const TigaBelas = () => {
   return (
     <div className="landing bg-gradient-to-tr from-[#be185d] via-[#f472b6] to-[#fbcfe8] text-white min-h-screen">
       <div className="body flex flex-col justify-center items-center min-h-screen">
-        <img src={gif} alt="" className="w-[150px] rounded-lg mb-[30px] md:w-[200px]" />
+        <img src={gif} alt="" className="w-[200px] rounded-lg mb-[30px] md:w-[200px]" />
         <h2 className=" md:text-[22px] text-center font-semibold px-[20px] md:w-[800px]">
           kali ke berapa ini akhirnya aku merasakan kesenangan yang luar biasa karena bisa makan bareng kamu trus walopun ini pas itu kondisi aku agak boncos diajakin makan ayce WKWKWKWK tapi aku happy bgt
         </h2>
