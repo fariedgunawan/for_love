c3f2cf8fdad838ecee3514e00ff96fa003107969
fariedgunawan <124080208+fariedgunawan@users.noreply.github.com>
2025-01-18 12:35:31 +0700

    Update Sebelas.tsx

--- for_love/src/page/Sebelas.tsx
+++ for_love/src/page/Sebelas.tsx
@@ -5,7 +5,7 @@ const Sebelas = () => {
   return (
     <div className="landing bg-gradient-to-tr from-[#be185d] via-[#f472b6] to-[#fbcfe8] text-white min-h-screen">
       <div className="body flex flex-col justify-center items-center min-h-screen">
-        <img src={gif} alt="" className="w-[150px] rounded-lg mb-[30px] md:w-[200px]" />
+        <img src={gif} alt="" className="w-[200px] rounded-lg mb-[30px] md:w-[200px]" />
         <h2 className=" md:text-[22px] text-center font-semibold px-[20px] md:w-[800px]">dah mulai nih keknya kemana2 bedua nih cieee... WKWKWKWKWKWKEK</h2>
         <button className="btn h-[10px] px-[20px] bg-white border-none hover:bg-[#f472b6] mt-[30px] text-[#be185d]" onClick={() => navigate("/12")}>
           next
